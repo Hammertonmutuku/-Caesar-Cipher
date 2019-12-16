d64327cd2425a4a15011aec0c9f2e2e660a92040
Hammertonmutuku <lthamma3@gmail.com>
2019-12-16 11:29:08 +0300

    introduced the first method and set out the if conditions

--- src/main/java/Cipher.java
+++ src/main/java/Cipher.java
@@ -1,4 +1,14 @@
 public class Cipher {
+
+    public static String encrypt(String plainTest, int shift){
+         if (shift >26){
+             shift = shift%26;
+         }
+         else if(shift<0) {
+             shift = (shift % 26) + 26;
+         }
+    }
+
     public static void main(String[] args) {
     }
 }
