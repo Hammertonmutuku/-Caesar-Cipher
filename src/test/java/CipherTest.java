56cbcb0ba07a29365f839bc4434f609d3cc964f9
Hammertonmutuku <lthamma3@gmail.com>
2019-12-17 11:51:37 +0300

    added the test

--- src/test/java/CipherTest.java
+++ src/test/java/CipherTest.java
@@ -1,11 +1,15 @@
-import org.junit.Test;
+import org.junit.After;
+import org.junit.Before;
 
 import static org.junit.Assert.*;
 
 public class CipherTest {
 
+    @Before
+    public void setUp() throws Exception {
+    }
 
-  }
-
-
+    @After
+    public void tearDown() throws Exception {
+    }
 }
