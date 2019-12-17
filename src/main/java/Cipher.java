2ec9bf17a65ad7bbb7413ff0b10ba5ac1b35de20
Hammertonmutuku <lthamma3@gmail.com>
2019-12-17 09:35:51 +0300

    added a string builder function to the cipher.java class

--- src/main/java/Cipher.java
+++ src/main/java/Cipher.java
@@ -1,3 +1,8 @@
 public class Cipher {
     String alphabetUpper = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
     String alphabetLower = alphabetUpper.toLowerCase();
+
+    public String encrypt(String input, int key) {
+        StringBuilder encrypted = new StringBuilder(input);
+        String shiftedAlphabetUpper = alphabetUpper.substring(key) + alphabetUpper.substring(0, key);
+        String shiftedAplhabetLower = alphabetLower.substring(key) + alphabetLower.substring(0, key);
