1655f8b1d0938dd8eb6614ecc426976952ed5779
Hammertonmutuku <lthamma3@gmail.com>
2019-12-17 09:37:42 +0300

    added the method

--- src/main/java/Cipher.java
+++ src/main/java/Cipher.java
@@ -6,3 +6,17 @@ public class Cipher {
         StringBuilder encrypted = new StringBuilder(input);
         String shiftedAlphabetUpper = alphabetUpper.substring(key) + alphabetUpper.substring(0, key);
         String shiftedAplhabetLower = alphabetLower.substring(key) + alphabetLower.substring(0, key);
+        for (int i = 0; i < encrypted.length(); i++) {
+            char currentChar = encrypted.charAt(i);
+
+            if (alphabetUpper.indexOf(currentChar) != -1) {
+                int indexOfChar = alphabetUpper.indexOf(currentChar);
+                char newChar = shiftedAlphabetUpper.charAt(indexOfChar);
+                encrypted.setCharAt(i, newChar);
+            }
+            else if (alphabetLower.indexOf(currentChar) != -1) {
+                int indexOfChar = alphabetLower.indexOf(currentChar);
+                char newChar = shiftedAplhabetLower.charAt(indexOfChar);
+                encrypted.setCharAt(i, newChar);
+            }
+        }
