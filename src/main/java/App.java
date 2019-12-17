153ae0040406afb9d343107c6661033312a356e4
Hammertonmutuku <lthamma3@gmail.com>
2019-12-17 12:17:50 +0300

    added the while loop

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -4,21 +4,24 @@ public class App {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         Cipher newCipher = new Cipher();
-        System.out.println("Enter (1) for encryption or (2) for decryption");
+        while (true) {
+            System.out.println("Enter (1) for encryption or (2) for decryption");
         int option = sc.nextInt();
-        if (option == 1) {
-            System.out.println("Enter plaintext to be encrypted: ");
-            String plaintext = sc.next();
-            System.out.println("Enter encryption key: ");
-            int key = sc.nextInt();
-            System.out.println("Your Encrtypted text is: "+newCipher.encrypt(plaintext, key));
-        }
-        else if (option == 2) {
-            System.out.println("Enter encrypted text for decryption: ");
-            String plaintext = sc.next();
-            System.out.println("Enter the decryption key: ");
-            int key = sc.nextInt();
-            System.out.println("Your decrypted text is: "+newCipher.encrypt(plaintext, 26-key));
+
+            if (option == 1) {
+                System.out.println("Enter plaintext to be encrypted: ");
+                String plaintext = sc.next();
+                System.out.println("Enter encryption key: ");
+                int key = sc.nextInt();
+                System.out.println("Your Encrtypted text is: " + newCipher.encrypt(plaintext, key));
+            }
+            else if (option == 2) {
+                System.out.println("Enter encrypted text for decryption: ");
+                String plaintext = sc.next();
+                System.out.println("Enter the decryption key: ");
+                int key = sc.nextInt();
+                System.out.println("Your decrypted text is: " + newCipher.encrypt(plaintext, 26 - key));
+            }
         }
     }
 }
